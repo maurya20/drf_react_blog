a418fd41ac6d0582d6b3fb851a88d24bd01e6561
maurya20 <mukesh.ice17@gmail.com>
2020-09-24 10:12:12 +0530

    Blogs list by only APIView get and post from function under class

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path, include
-from blog import views
-from rest_framework import routers
+from .views import Blogslist
 
-
-
-
-    
-router = routers.DefaultRouter()
-router.register(r'bapi', views.BlogViewSet)
 
 
 urlpatterns = [
-    path('blogs/', views.blogs, name="blogs"),
-    path('', include(router.urls)),
-    path('blogs_list/', views.blogs_list, name="blogs_list"),
-    # path('snippets/<int:pk>/', views.snippet_detail),
+    path('blogs/',Blogslist.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,40 +1,20 @@
-from rest_framework import status
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from blog.models import Blog
-from blog.serializers import BlogSerializer
-from django.db import models
-from rest_framework import viewsets
-from django.http import JsonResponse
-from django.shortcuts import render
 
 
 
+class Blogslist(APIView):
+    permission_classes = []
+    authentication_classes = []
+    
+    def get(self, request, format=None):
+        qs = Blog.objects.all()
+        serializer = BlogSerializer(qs, many=True)
+        return Response(serializer.data)
 
-def blogs_list(request):
-    data = {
-        "id": 5,
-        "title": "bhalu",
-        "category": "Electronics",
-        "content": "sergemeyill dfhtfjtmk6u srgrernnse5h rgrerny",
-        "created_on": "2020-09-21T10:07:42.274442Z",
-        "author": 2
-    }
-    return JsonResponse(data)
-
-
-
-
-
-class BlogViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.all().filter().order_by('-id')
-    serializer_class = BlogSerializer
-
-
-
-@api_view(["GET"])
-def blogs(request):
-    blogs = Blog.objects.all()
-    if request.method == "GET":
-        serializer = BlogSerializer(blogs)
+    def post(self, request, format=None):
+        qs = Blog.objects.all()
+        serializer = BlogSerializer(qs, many=True)
         return Response(serializer.data)
