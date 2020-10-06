2215321293afc4087f7f0813a71873bc3f6eaef3
maurya20 <mukesh.ice17@gmail.com>
2020-10-06 18:43:02 +0530

    trying multiple fetch

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import '../App.css'
 
 export class Detail extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ export class Detail extends Component {
       data: [],
       loaded: false,
       placeholder: "Loading",
+      user:[]
     };
   }
 
@@ -37,14 +39,42 @@ export class Detail extends Component {
         });
       });
   }
- 
+ //   for User details
+ // eslint-disable-next-line
+//  componentDidMount() {
+//   fetch(`http://127.0.0.1:8000/userdetail/2`, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `JWT ${localStorage.getItem("token")}`,
+//     },
+//   })
+//     .then((response) => {
+//       if (response.status > 400) {
+//         return this.setState(() => {
+//           return { placeholder: "Something went wrong!" };
+//         });
+//       }
+//       return response.json();
+//     })
+//     .then((result) => {
+//       this.setState(() => {
+//         return {
+//           user:result
+      
+//         };
+//       });
+//     });
+// }
   render() {
     
     
     return (
       <div className="container">
-        <h3 className="blog-heading">{this.state.data.title}</h3>
+        <div className="blog-heading">
+        <h5>{this.state.data.title} </h5><h6 style={{color:'red'}}>Category:{this.state.data.category}</h6>
+        </div>
     <p>Published on: {this.state.data.created_on}</p>
+    <h3>Author:{this.state.user.username}</h3>
     <div className="blog-image">
           <img src='https://source.unsplash.com/random'alt="Some Blog Pic" width="100%" height="500" />
         </div>
