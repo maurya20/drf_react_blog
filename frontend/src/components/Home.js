52f24bd5c744583113d4540bc27673ce1ee3bcef
maurya20 <mukesh.ice17@gmail.com>
2020-10-13 16:37:07 +0530

    Dynamic category link given on home page for category.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -70,7 +70,7 @@ this.state = {
             <div className="caption">
             
                     <h6 style={{color:"blue"}}><Link to={`/detail/?id=${blog.id}&user=${blog.author}`}>{blog.title}</Link></h6>
-                    <h6>Category: <Link to={'/about'}>{blog.category}</Link> </h6>
+                    <h6>Category: <Link to={`/bycategory/?c=${blog.category}`}>{blog.category}</Link> </h6>
                    
                   </div>
             </Card.Footer>
