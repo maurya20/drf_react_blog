7461e6c638861dede294b218ed38813185d23cec
maurya20 <mukesh.ice17@gmail.com>
2020-10-20 16:09:04 +0530

    Pagination update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -35,7 +35,7 @@ this.state = {
         .then((data) => {
           this.setState(() => {
             return {
-              data,
+              data:data.results,
               loaded: true,
             };
           });
@@ -79,6 +79,21 @@ this.state = {
             );
           })}
         </div>
+        <nav aria-label="...">
+  <ul className="pagination">
+    <li className="page-item disabled">
+      <a className="page-link" href="#" tabindex="-1">Previous</a>
+    </li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    <li className="page-item active">
+      <a className="page-link" href="#">2 <span className="sr-only">(current)</span></a>
+    </li>
+    <li className="page-item"><a className="page-link" href="#">3</a></li>
+    <li className="page-item">
+      <a className="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
         </div>
       );
     }
