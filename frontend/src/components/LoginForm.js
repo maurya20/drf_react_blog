a69fbbeb84b7e2604cf825a5cc4ac80eaa06feed
maurya20 <mukesh.ice17@gmail.com>
2020-11-29 11:01:47 +0530

    context api used Login component

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,60 +1,79 @@
-import React from "react";
+import React, {useContext, useState} from 'react'
+import {BlogContext} from '../store/BlogContext'
+import axios from 'axios'
 
 
-class LoginForm extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
 
-  handle_change = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState((prevstate) => {
-      const newState = { ...prevstate };
-      newState[name] = value;
-      return newState;
-    });
-  };
 
-  render() {
-    return (
-      <div className="container">
-        <br/>
-        <br/>
-        <h3>Login Here!!</h3>
-        <form
-          className="form-inline"
-          onSubmit={(e) => this.props.handle_login(e, this.state)}
-        >
-          <label className="mb-2 mr-sm-2">Username:</label>
-          <input
-            type="text"
-            className="form-control mb-2 mr-sm-2"
-            name="username"
-            value={this.state.username}
-            onChange={this.handle_change}
-          />
-          <label className="mb-2 mr-sm-2">Password:</label>
-          <input
-            className="form-control mb-2 mr-sm-2"
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handle_change}
-          />
 
-          <button type="submit" className="btn btn-primary mb-2">
-            Submit
-          </button>
-        </form>
-        <br/>
-        <br/>
-      </div>
-    );
+const LoginForm = () => {
+  const [appState, setAppState] = useContext(BlogContext)
+  const [user, setUser] = useState("")
+  const [password, setPassword] = useState("")
+  const [msg, setMsg] = useState("")
+
+  const handle_login = (e)=>{
+    const data = {username:user,password:password}
+      e.preventDefault();
+    fetch("http://localhost:8000/api/token-auth/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok){setMsg({msg:"Invalid Credentials ❌ "})
+        setTimeout(()=>{
+          setMsg({msg:""})
+        },4000)
+      }
+        else return response.json();
+      })
+      .then((json) => {
+        localStorage.setItem("blogtoken", json.token);
+
+        setAppState({
+          logged_in: true,
+          username: json.user.username,
+          uid: json.user.id,
+        });
+      });
+
   }
+  return (
+    <div className="container">
+      <br/>
+      <br/>
+      <h3>Login Here!!</h3>
+      <form
+        className="form-inline"
+        onSubmit={(e) => handle_login(e)}
+      >
+        <label className="mb-2 mr-sm-2">Username:</label>
+        <input
+          type="text"
+          className="form-control mb-2 mr-sm-2"
+          name="username"
+          onChange={(e)=>setUser(e.target.value)}
+        />
+        <label className="mb-2 mr-sm-2">Password:</label>
+        <input
+          className="form-control mb-2 mr-sm-2"
+          type="password"
+          name="password"
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button type="submit" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+      <br/>
+      <br/>
+    </div>
+  )
 }
 
-export default LoginForm;
-
+export default LoginForm
 
